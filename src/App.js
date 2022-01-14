5227e2c98ac2ca0500698133bb0c919c10a479d2
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-01-14 14:13:18 -0600

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import './App.css';
-import './fonts/Magilio.ttf';
-import { Container } from 'react-bootstrap';
-import background from "./images/background.jpg"
+import './App.css'
+// import { Container } from 'react-bootstrap'
+import bg from "./images/bg.jpg"
+import lexi from "./images/lexi.png"
 
 function App() {
   return (
-    <div style={{backgroundImage: `url(${background})`}} className="App">
-        <Container class="largespaceabove">
-          <h1 id="logo">AS</h1>
-        </Container>
-        <h5 id="test">
-          Welcome to my portfolio
-        </h5>
-        <h1 id="heading">Alejandra Sandoval</h1>
+    <div style={{backgroundImage: `url(${bg})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} className="App">
+        <div class="spacer"/>
+
+        <h1 class="white ogg medium spacebelow">Hey, I'm Alejandra</h1>
+        <h5 class="white universcondensed xsmall spacebelow">A full stack engineer with a passion for design.</h5>
+        <img src={lexi} style={{width: '40%', height: 'auto'}} alt="Lexi"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+        <div class="spacer"/>
+    
+
+
     </div>
   );
 }
