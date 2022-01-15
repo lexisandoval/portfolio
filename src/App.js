3db91ba243bfe90201d00744a4f32765ee8db0cc
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-01-15 15:42:47 -0600

    Fixing mobile UI

--- src/App.js
+++ src/App.js
@@ -25,26 +25,24 @@ function App() {
         </Row>
       </Container>
       <div class="spacer" />
-      <Container class="">
-        <h5 class="white ogg medium spaceabove spacebelow">Get In Touch</h5>
-        <h6 class="contmargin universcondensed small white">If you're interested in working with me or just want to say hello, please feel free to email me or connect via my social links below!</h6>
-        <a href="mailto:lexi.sandoval9@gmail.com">
-          <Button id="emailBtn" class="spaceabove">SEND ME AN EMAIL</Button>
-        </a>
-        <br />
+      <h5 class="white ogg medium spaceabove spacebelow">Get In Touch</h5>
+      <h6 class="contmargin universcondensed small white">If you're interested in working with me or just want to say hello, please feel free to email me or connect via my social links below!</h6>
+      <a href="mailto:lexi.sandoval9@gmail.com">
+        <Button id="emailBtn" class="spaceabove">SEND ME AN EMAIL</Button>
+      </a>
+      <br />
 
-        <a href="https://www.linkedin.com/in/alejandramsandoval/" target="_blank" rel="noreferrer">
-          <img src={linkedin} class="socials" alt="linkedin" />
-        </a>
-        <a href="https://github.com/lexisandoval" target="_blank" rel="noreferrer">
+      <a href="https://www.linkedin.com/in/alejandramsandoval/" target="_blank" rel="noreferrer">
+        <img src={linkedin} class="socials" alt="linkedin" />
+      </a>
+      <a href="https://github.com/lexisandoval" target="_blank" rel="noreferrer">
 
-          <img src={github} class="socials" alt="github" />
-        </a>
+        <img src={github} class="socials" alt="github" />
+      </a>
 
-        <div class="spacer" />
-        <div class="spacer" />
+      <div class="spacer" />
+      <div class="spacer" />
 
-      </Container>
     </Container>
   )
 }
