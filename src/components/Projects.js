cbb84b129754f5b63799c6b50af5ed038cc2761c
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-01-26 16:41:41 -0600

    Small styling update, added project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,9 @@ function Projects() {
       <br />
       <h2 class="white ogg large spacebelow">Projects</h2>
 
+      <Container className="max800">
       <h6 className="white ogg medium smallspacebelow">Windy City Rooftops</h6>
+      <h6 className="white universcondensed small">We've compiled a list of Chicago's best rooftop bars and restaurants so you can start planning your next visit. Cheers to the Windy City!</h6><br />
       <div className="img__wrap mx-auto">
         <img src={wcr} className="projects" alt='Windy City Rooftops' />
         <div className="img__description_layer">
@@ -23,18 +25,22 @@ function Projects() {
       <a href="https://windycityrooftops.netlify.app/" target="_blank" rel="noreferrer" className="nounderline">
         <Button id="emailBtn" className="spaceabove spacebelow">VISIT WEBSITE</Button>
       </a>
+      </Container>
       <div className="spacer" />
 
-      <h6 className="white ogg medium smallspacebelow">RateMyCourses</h6>
-      <div className="img__wrap mx-auto">
-      <img src={rmc} className="projects" alt='RateMyCourses' />
-        <div className="img__description_layer">
-          <h6 className="horizontalspacer universcondensed small white">Built with Ruby on Rails for both frontend and backend</h6>
-        </div>
-      </div><br />
-      <a href="https://ratemy-courses.herokuapp.com/" target="_blank" rel="noreferrer" className="nounderline">
-        <Button id="emailBtn" className="spaceabove spacebelow">VISIT WEBSITE</Button>
-      </a>
+      <Container className="max800">
+        <h6 className="white ogg medium smallspacebelow">RateMyCourses</h6>
+        <h6 className="white universcondensed small">Rate, review, and rave about courses at UW-Madison. We're your trusted source for honest opinions from hundreds of students just like you!</h6><br />
+        <div className="img__wrap mx-auto">
+          <img src={rmc} className="projects" alt='RateMyCourses' />
+          <div className="img__description_layer">
+            <h6 className="horizontalspacer universcondensed small white">Built with Ruby on Rails for both frontend and backend</h6>
+          </div>
+        </div><br />
+        <a href="https://ratemy-courses.herokuapp.com/" target="_blank" rel="noreferrer" className="nounderline">
+          <Button id="emailBtn" className="spaceabove spacebelow">VISIT WEBSITE</Button>
+        </a>
+      </Container>
       <div className="spacer" />
 
       <div className="footer">
