83a2df8283f48cf0a52c99e72f9f62e85b9e2cd2
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-01-26 16:34:24 -0600

    Small UI update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,7 +17,7 @@ function Projects() {
       <div className="img__wrap mx-auto">
         <img src={wcr} className="projects" alt='Windy City Rooftops' />
         <div className="img__description_layer">
-          <h6 className="contmargin universcondensed small white">Built with Javascript for frontend and Ruby on Rails for backend</h6>
+          <h6 className="horizontalspacer universcondensed small white">Built with Javascript for frontend and Ruby on Rails for backend</h6>
         </div>
       </div><br />
       <a href="https://windycityrooftops.netlify.app/" target="_blank" rel="noreferrer" className="nounderline">
@@ -29,7 +29,7 @@ function Projects() {
       <div className="img__wrap mx-auto">
       <img src={rmc} className="projects" alt='RateMyCourses' />
         <div className="img__description_layer">
-          <h6 className="contmargin universcondensed small white">Built with Ruby on Rails for both frontend and backend</h6>
+          <h6 className="horizontalspacer universcondensed small white">Built with Ruby on Rails for both frontend and backend</h6>
         </div>
       </div><br />
       <a href="https://ratemy-courses.herokuapp.com/" target="_blank" rel="noreferrer" className="nounderline">
