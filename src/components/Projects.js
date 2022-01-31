82bf4cabb720946c2caee5273cf9dbbbb3433a75
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-01-31 15:48:30 -0600

    Added new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,7 @@ import Footer from "./Footer"
 import bg from "../images/bg.jpg"
 import wcr from "../images/wcr.png"
 import rmc from "../images/rmc.png"
+import lexislocker from "../images/lexislocker.png"
 
 function Projects() {
   useEffect(() => {
@@ -30,12 +31,27 @@ function Projects() {
       <div className="smmobilespacer" />
 
       <Container className="spaceabove max800">
+        <h6 className="white ogg medium smallspacebelow">Lexi's Locker</h6>
+        <h6 className="white universcondensed small">Discover unique secondhand items that are sure to make you stand out from the crowd. Shop sustainably, shop the Locker!</h6><br />
+        <div className="img__wrap mx-auto">
+          <img src={lexislocker} className="projects" alt='Lexis Locker' />
+          <div className="img__description_layer">
+            <h6 className="horizontalspacer universcondensed small white">Built using React, Spring Boot, Bootstrap, MySQL, Git, and AWS</h6>
+          </div>
+        </div><br />
+        <a href="https://lexislocker.netlify.app" target="_blank" rel="noreferrer" className="nounderline">
+          <Button id="websiteBtn" className="spaceabove spacebelow">VISIT WEBSITE</Button>
+        </a>
+      </Container>
+      <div className="spacer mobilespacer" />
+
+      <Container className="spaceabove max800">
         <h6 className="white ogg medium smallspacebelow">Windy City Rooftops</h6>
         <h6 className="white universcondensed small">We've compiled a list of Chicago's best rooftop bars and restaurants so you can start planning your next visit. Cheers to the Windy City!</h6><br />
         <div className="img__wrap mx-auto">
           <img src={wcr} className="projects" alt='Windy City Rooftops' />
           <div className="img__description_layer">
-            <h6 className="horizontalspacer universcondensed small white">Built the frontend with Javascript and backend with Ruby on Rails</h6>
+            <h6 className="horizontalspacer universcondensed small white">Built using JavaScript, HTML, CSS, Ruby on Rails, and Git</h6>
           </div>
         </div><br />
         <a href="https://windycityrooftops.netlify.app/" target="_blank" rel="noreferrer" className="nounderline">
@@ -50,7 +66,7 @@ function Projects() {
         <div className="img__wrap mx-auto">
           <img src={rmc} className="projects" alt='RateMyCourses' />
           <div className="img__description_layer">
-            <h6 className="horizontalspacer universcondensed small white">Built the frontend and the backend with Ruby on Rails</h6>
+            <h6 className="horizontalspacer universcondensed small white">Built using Ruby on Rails, SQLite, Git, and MVC architecture</h6>
           </div>
         </div><br />
         <a href="https://ratemy-courses.herokuapp.com/" target="_blank" rel="noreferrer" className="nounderline">
