97b571508a5266b3668a26ab4c6dff2aa10f1f7b
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-01-31 10:26:28 -0600

    Wording change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,7 +34,7 @@ function Projects() {
         <div className="img__wrap mx-auto">
           <img src={wcr} className="projects" alt='Windy City Rooftops' />
           <div className="img__description_layer">
-            <h6 className="horizontalspacer universcondensed small white">Built with Javascript for frontend and Ruby on Rails for backend</h6>
+            <h6 className="horizontalspacer universcondensed small white">Built the frontend with Javascript and backend with Ruby on Rails</h6>
           </div>
         </div><br />
         <a href="https://windycityrooftops.netlify.app/" target="_blank" rel="noreferrer" className="nounderline">
@@ -49,7 +49,7 @@ function Projects() {
         <div className="img__wrap mx-auto">
           <img src={rmc} className="projects" alt='RateMyCourses' />
           <div className="img__description_layer">
-            <h6 className="horizontalspacer universcondensed small white">Built with Ruby on Rails for both frontend and backend</h6>
+            <h6 className="horizontalspacer universcondensed small white">Built the frontend and backend with Ruby on Rails</h6>
           </div>
         </div><br />
         <a href="https://ratemy-courses.herokuapp.com/" target="_blank" rel="noreferrer" className="nounderline">
