2a84dc276c87cea2882b2a510435ed0eafb55808
Lexi Sandoval <lexi.sandoval9@gmail.com>
2022-02-01 11:32:08 -0600

    Styling updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Container, Button } from 'react-bootstrap'
-import { useMediaQuery } from 'react-responsive'
 
 import NavBar from "./NavBar"
 import Footer from "./Footer"
@@ -14,8 +13,6 @@ function Projects() {
     window.scrollTo(0, 0)
   }, [])
 
-  const isMobile = useMediaQuery({ query: `(max-width: 760px)` })
-
   return (
     <Container style={{ backgroundImage: `url(${bg})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="App">
       <NavBar />
@@ -28,7 +25,7 @@ function Projects() {
 
       <Container className="spaceabove max800">
         <h6 className="white ogg medium smallspacebelow">Lexi's Locker</h6>
-        <h6 className="white universcondensed small">Discover unique secondhand items that are sure to make you stand out from the crowd. Shop sustainably, shop the Locker!</h6><br />
+        <h6 className="white universcondensed small medspacebelow">Discover unique secondhand items that are sure to make you stand out from the crowd. Shop sustainably, shop the Locker!</h6>
         <div className="img__wrap mx-auto">
           <img src={lexislocker} className="projects" alt='Lexis Locker' />
           <div className="img__description_layer">
@@ -43,7 +40,7 @@ function Projects() {
 
       <Container className="spaceabove max800">
         <h6 className="white ogg medium smallspacebelow">Windy City Rooftops</h6>
-        <h6 className="white universcondensed small">We've compiled a list of Chicago's best rooftop bars and restaurants so you can start planning your next visit. Cheers to the Windy City!</h6><br />
+        <h6 className="white universcondensed small medspacebelow">We've compiled a list of Chicago's best rooftop bars and restaurants so you can start planning your next visit. Cheers to the Windy City!</h6>
         <div className="img__wrap mx-auto">
           <img src={wcr} className="projects" alt='Windy City Rooftops' />
           <div className="img__description_layer">
@@ -58,7 +55,7 @@ function Projects() {
 
       <Container className="max800">
         <h6 className="white ogg medium smallspacebelow">RateMyCourses</h6>
-        <h6 className="white universcondensed small">Rate, review, and rave about courses at UW-Madison. We're your trusted source for honest opinions from hundreds of students just like you!</h6><br />
+        <h6 className="white universcondensed small medspacebelow">Rate, review, and rave about courses at UW-Madison. We're your trusted source for honest opinions from hundreds of students just like you!</h6>
         <div className="img__wrap mx-auto">
           <img src={rmc} className="projects" alt='RateMyCourses' />
           <div className="img__description_layer">
